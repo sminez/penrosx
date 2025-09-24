9e574ff601e3bcd4078fc52eb90c3380a93e5d6c
Innes Anderson-Morrison <innes.andersonmorrison@gmail.com>
2025-09-24 07:05:08 +0100

    Clean start

--- build.rs
+++ build.rs
@@ -1,4 +1,8 @@
 pub fn main() {
+    println!("cargo:rustc-link-search=framework=/System/Library/PrivateFrameworks");
+
     println!("cargo:rustc-link-lib=framework=AppKit");
+    println!("cargo:rustc-link-lib=framework=CoreFoundation");
     println!("cargo:rustc-link-lib=framework=Foundation");
+    println!("cargo:rustc-link-lib=framework=SkyLight");
 }
