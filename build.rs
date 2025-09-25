85739b6b0caa612764635f9a25840a7b6f3cc976
Innes Anderson-Morrison <innes.andersonmorrison@gmail.com>
2025-09-25 13:03:45 +0100

    Remove obsolete build.rs

--- build.rs
+++
@@ -1,8 +0,0 @@
-pub fn main() {
-    println!("cargo:rustc-link-search=framework=/System/Library/PrivateFrameworks");
-
-    println!("cargo:rustc-link-lib=framework=AppKit");
-    println!("cargo:rustc-link-lib=framework=CoreFoundation");
-    println!("cargo:rustc-link-lib=framework=Foundation");
-    println!("cargo:rustc-link-lib=framework=SkyLight");
-}
